7e6d0314e50dda733112059cfd5b15faeeda34c6
aaaaus <ausluft@gmail.com>
2019-01-19 19:53:56 -0500

    began implementing post action for creating lease

--- src/components/UnitDetail.js
+++ src/components/UnitDetail.js
@@ -5,6 +5,13 @@ import { fetchLeases } from '../actions';
 
 class UnitDetail extends React.Component {
 
+  state = {
+    lessees: [],
+    newStartDate: '',
+    newEndDate: '',
+    rent: '',
+  }
+
   componentDidMount() {
     this.props.fetchLeases();
   }
@@ -17,6 +24,42 @@ class UnitDetail extends React.Component {
 
   multiRes(array) {
     return array.map(resident => this.nameMerge(resident)).join(', ')
+  }
+
+  //form
+  handleDateChange = (event) => {
+    console.log(event.target.value);
+    const date = new Date(event.target.value)
+    const epoch = date.getTime()
+    const unix_time = epoch/1000
+    console.log(unix_time); //send this to backend
+    //return this setState blah to blah
+  }
+
+  handleChange = (e) => {
+    console.log(this.state);
+    return this.setState({ [e.target.name]: e.target.value })
+  }
+
+  createNewLease() {
+    const nStart = this.state.newStartDate
+    const nEnd = this.state.newEndDate
+    const nRent = this.state.rent
+
+    if (nStart && nEnd && nRent) {
+        const nBody = {lease: {start_date: nStart, end_date: nEnd, rent: nRent}}
+        fetch('http://localhost:3000/api/v1/sightings', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify(nBody)
+        })
+        .then(res => {
+          console.log(res)
+        })
+    }
   }
 
   renderContent() {
@@ -41,12 +84,25 @@ class UnitDetail extends React.Component {
       console.log("YOUR LEASE IS:", lease[0]);
 
       if (lease.length === 0) {
+        console.log("UNIT DETAIL STATE IS: ", this.state);
         return (
           <div>
             <h3>UnitDetail</h3>
             <p><em>No active lease for this unit</em></p>
             <button>Create New Lease</button>
+            <br /><br />
+            <form>
+              <label htmlFor="start-date">Start date </label>
+              <input type="date" name="newStartDate" id="start-date" onChange={this.handleDateChange}  /><br />
+              <label htmlFor="end-date">End date </label>
+              <input type="date" name="newEndDate" id="end-date" /><br />
+              <label htmlFor="end-date">Rent </label>
+              <input name="rent" placeholder="Rent" onChange={this.handleChange} value={this.state.rent}></input><br />
+              <button type="submit">Create</button>
+            </form>
           </div>
+
+
         )
       }
 
@@ -86,3 +142,47 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, { fetchLeases })(UnitDetail);
+
+
+// function createNewSighting() {
+//   const createForm = document.getElementById('create-form')
+//   const nEntity = createForm.querySelector('input[name=entity]').value
+//   const nImage = createForm.querySelector('input[name=image-url]').value
+//   const nDescription = createForm.querySelector('textarea[name=description]').value
+//   const nLat = document.getElementById('create-coords').innerHTML.split('<br>')[0]
+//   const nLong = document.getElementById('create-coords').innerHTML.split('<br>')[1]
+//   const nMonsterId = createForm.querySelector('select').value
+//
+//   if (nEntity && nImage && nDescription && nLat && nLong && nMonsterId) {
+//     const nBody = {sighting: {entity: nEntity, image: nImage, description: nDescription, lat: nLat, long: nLong, monster_id: nMonsterId}}
+//     fetch('http://localhost:3000/api/v1/sightings', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//       },
+//       body: JSON.stringify(nBody)
+//     })
+//     .then(res => res.json())
+//     .then(res => {
+//       console.log(res)
+//       if (res.id) {
+//         const nSighting = new Sighting(res)
+//         loadAllSightings()
+//         .then(() => {
+//           deleteMarkers()
+//           renderSightings()
+//           resetCreate()
+//           renderInfoSidebar(res)
+//         })
+//
+//       }
+//     })
+//   } else {
+//     toggleInvisible(document.getElementById('open-create'))
+//     setTimeout(() => {
+//       dropInCreate(createForm)
+//       alert('You must fill out all forms!')
+//     },1050)
+//   }
+// }
